785045688218cfe2776fab1b93026f5b4252666a
kheireddine chebicheb <kheireddine.chebicheb@univ-tiaret.dz>
2026-04-06 15:08:42 +0100

    Create all file note and bd and update npm

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return "welcome to Laravel";
 });
 Route::get('/about',function(){
     return "about page";
